99c82b06677df6ea11f2f2d011a5ca06dfe3c277
Egor Bondarevskii <egorbondarevskii@MacBook-Pro-Egor.local>
2025-07-24 12:47:00 +0300

    fix build errors

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import "./styles/global.css";
 import { AppRoutes } from "./AppRoutes";
 
 const App: React.FC = () => {
-  const { authenticated, user, logout, ready } = usePrivy();
+  const { authenticated, user, ready } = usePrivy();
 
   const manifestUrl = `https://gist.githubusercontent.com/z0rats/370cce01adcc582a1177ffbd058172bf/raw/c8f7089ed5fb1e7c7b024e0f8e2ef43bfa62bd9f/tonconnect-manifest.json`;
 

75a55a7766bb2e5b4cc16fc6c0ba1b9a07e35006
Egor Bondarevskii <egorbondarevskii@MacBook-Pro-Egor.local>
2025-07-25 16:22:28 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -20,11 +20,12 @@ export default defineConfig({
     hmr: {
       clientPort: Number(process.env.VITE_PORT) || 3000,
     },
-    allowedHosts: ["496b8cf03793.ngrok-free.app"],
+    allowedHosts: ["31754fdd6cf2.ngrok-free.app"],
   },
   preview: {
     port: Number(process.env.VITE_PORT) || 3000,
     host: "0.0.0.0",
+    allowedHosts: ["31754fdd6cf2.ngrok-free.app"],
   },
   cacheDir: ".vite", // Make sure the cache is stored in a persistent location
   build: {

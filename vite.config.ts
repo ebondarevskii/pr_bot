71a6fce7c4666e585d1d065fe1c5b3cecfabdfb2
Egor Bondarevskii <egorbondarevskii@MacBook-Pro-Egor.local>
2025-07-25 14:31:03 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
   plugins: [
+    basicSsl(),
     react(),
     NodeGlobalsPolyfillPlugin({
       buffer: true,

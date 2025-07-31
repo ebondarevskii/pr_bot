f3435d052ef353522cdf3ac206efbf14f106d7f5
Egor Bondarevskii <egorbondarevskii@MacBook-Pro-Egor.local>
2025-07-31 17:22:22 +0300

    fix

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,11 +1,17 @@
 import { Header } from "@/components/UI/Header";
 import { BalanceCard } from "./components/BalanceCard";
 import { PositionsCard } from "./components/PositionsCard/PositionsCard";
+import { usePrivyAuth } from "@/hooks/usePrivyAuth";
+import { Button } from "@/components/UI/Button";
 
 export const Portfolio = () => {
+  const { logout } = usePrivyAuth();
+
   return (
     <div className="pt-[25px] px-2.5 pb-16">
       <Header>Portfolio</Header>
+
+      <Button onClick={logout}>logout</Button>
 
       <BalanceCard />
 
